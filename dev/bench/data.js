0507a17e8a42c88da6618a36814dcca0ab00d2af
github-action-benchmark <github@users.noreply.github.com>
2025-04-24 09:22:30 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for 2bc3924ca9153874aefc07bc1e93ffb2f15cb961

---
+++ dev/bench/data.js
@@ -0,0 +1,37 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1745486550707,
+  "repoUrl": "https://github.com/Tanv33/godot-solana-sdk",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "tanveer.khan2692000@gmail.com",
+            "name": "tanv33",
+            "username": "Tanv33"
+          },
+          "committer": {
+            "email": "tanveer.khan2692000@gmail.com",
+            "name": "tanv33",
+            "username": "Tanv33"
+          },
+          "distinct": true,
+          "id": "2bc3924ca9153874aefc07bc1e93ffb2f15cb961",
+          "message": "VersionedTransaction added in wallet adapter",
+          "timestamp": "2025-04-24T14:13:57+05:00",
+          "tree_id": "0113c899403788cf8c78cbfa32dbc929aafcd554",
+          "url": "https://github.com/Tanv33/godot-solana-sdk/commit/2bc3924ca9153874aefc07bc1e93ffb2f15cb961"
+        },
+        "date": 1745486549695,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Simple Transaction Performance Index",
+            "value": 0.366438356164384,
+            "unit": "Percent"
+          }
+        ]
+      }
+    ]
+  }
+}
